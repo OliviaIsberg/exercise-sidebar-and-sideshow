cd00ad52cea99357e91ecabcfa40651fb9f42666
Olivia isberg <oliviaisberg@gmail.com>
2021-11-16 14:32:22 +0100

    Hamburgemeny funkar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let delay = 5000;
+    let delay = 5000;
 let scroller = document.getElementById('scroller');
 let images = document.getElementById('scroller').getElementsByTagName('img');
 
@@ -8,3 +8,22 @@ window.setInterval(function () {
     index = (index + 1) % images.length;
     scroller.style.transform = 'translateX(' + (index * -100) + 'vw)';
 }, delay);
+
+
+const burgerMenu = document.getElementById('burgerIcon');
+const sideMenu = document.getElementById('sidemenu');
+const aside = document.getElementById('aside');
+
+function showMenu() {
+    sideMenu.classList.toggle('show');
+    burgerMenu.style.display = 'none';
+}
+
+burgerMenu.addEventListener('click', showMenu);
+
+sideMenu.addEventListener('click', function(e) {
+    if(e.target === sideMenu) {
+        sideMenu.classList.toggle('show');
+        burgerMenu.style.display = 'inline';
+    }
+}, false);
